f40d6266e34edaf513d970b1fba2af027fdf789d
Alexander Garcia <javier.garci@gmail.com>
2021-10-05 14:57:33 -0500

    Se agregan archivos para generacion de war

--- src/main/java/com/gnp/hackerrank/HackerRank/HackerRankApplication.java
+++ src/main/java/com/gnp/hackerrank/HackerRank/HackerRankApplication.java
@@ -2,6 +2,7 @@ package com.gnp.hackerrank.HackerRank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HackerRankApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HackerRankApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackerRankApplication.class, args);
