a949cac4055f57b878ef15330063c99020208e83
Alexander Garcia <javier.garci@gmail.com>
2021-10-05 15:24:18 -0500

    Se agrega README

--- src/main/java/com/gnp/hackerrank/HackerRank/HackerRankApplication.java
+++ src/main/java/com/gnp/hackerrank/HackerRank/HackerRankApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HackerRankApplication extends SpringBootServletInitializer {
